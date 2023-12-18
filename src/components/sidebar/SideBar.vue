8b6a56c879b1099696cfcdd50009bdb89e06e171
Anees Muzzafer <aneesmuzzafer@gmail.com>
2023-12-18 19:13:41 +0530

    ✨ Sidebar: update UI

--- src/components/sidebar/SideBar.vue
+++ src/components/sidebar/SideBar.vue
@@ -10,13 +10,23 @@
 
 import { Routes } from '@/router/routes';
 import { RouterLink } from 'vue-router';
+import { Icon } from '@iconify/vue';
 
 </script>
 
 <template>
     <div class="flex flex-col h-screen w-80">
+        <div class="flex items-center gap-4 pt-10 pb-4 pl-4">
+            <img src="./../../assets/logo.jpg" class="w-10 h-10">
+            <span class="text-2xl font-semibold text-slate-800">Harmona</span>
+        </div>
         <div v-for="item in Routes" :key="item.key">
-           <RouterLink :to="item.key"> {{ item.label }}</RouterLink>
+            <RouterLink :to="item.key">
+                <div class="flex items-center gap-4 py-4 pl-8 hover:bg-purple-50">
+                    <Icon :icon="item.icon" />
+                    <span class="font-semibold"> {{ item.label }}</span>
+                </div>
+            </RouterLink>
         </div>
     </div>
 </template>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -15,15 +15,15 @@ export enum Route {
 }
 
 export enum RouteName {
-    DASHBOARD = "dashboard",
-    HISTORY = "history",
-    REQUESTS = "requests",
-    PROFILE = "profile",
+    DASHBOARD = "Dashboard",
+    HISTORY = "History",
+    REQUESTS = "Requests",
+    PROFILE = "Profile",
 }
 
 export const Routes = [
-    { key: Route.DASHBOARD, label: RouteName.DASHBOARD },
-    { key: Route.HISTORY, label: RouteName.HISTORY },
-    { key: Route.REQUESTS, label: RouteName.REQUESTS },
-    { key: Route.PROFILE, label: RouteName.PROFILE },
+    { key: Route.DASHBOARD, label: RouteName.DASHBOARD, icon: "ic:round-dashboard" },
+    { key: Route.HISTORY, label: RouteName.HISTORY, icon: "mdi:calendar" },
+    { key: Route.REQUESTS, label: RouteName.REQUESTS, icon: "ant-design:form-outlined" },
+    { key: Route.PROFILE, label: RouteName.PROFILE, icon: "iconamoon:profile-circle-light" },
 ];
