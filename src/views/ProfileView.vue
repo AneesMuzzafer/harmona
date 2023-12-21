807c43f38507da14c7c3cb2511ac185a269a82d4
Anees Muzzafer <aneesmuzzafer@gmail.com>
2023-12-22 00:29:24 +0530

    ✏️ Fix typos

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -29,25 +29,25 @@ const userInfo = ref({
 
 <template>
     <div class="flex flex-col px-10 pt-4 m-6 bg-white rounded-2xl">
-        <div class="flex gap-24 py-6 border-gray-200 border0b">
+        <div class="flex gap-32 py-6 border-gray-200">
             <InputMain class="flex-1" label="First Name" v-model="userInfo.firstName" placeholder="First Name" required />
             <InputMain class="flex-1" label="Last Name" v-model="userInfo.lastName" placeholder="Last Name" />
             <div class="flex-1"></div>
         </div>
-        <div class="flex gap-24 py-6 border-gray-200 border0b">
+        <div class="flex gap-32 py-6 border-gray-200">
             <InputMain class="flex-1" label="Username" v-model="userInfo.username" placeholder="Username" />
             <InputMain class="flex-1" label="Email" v-model="userInfo.email" placeholder="Email" type="email" />
             <InputMain class="flex-1" label="Phone" v-model="userInfo.phone" placeholder="Phone" type="number" />
         </div>
-        <div class="flex gap-24 py-6 border-gray-200 border0b">
+        <div class="flex gap-32 py-6 border-gray-200">
             <InputMain class="flex-1" label="Address" v-model="userInfo.address" placeholder="Address" />
         </div>
-        <div class="flex gap-24 py-6 border-gray-200 border0b">
+        <div class="flex gap-32 py-6 border-gray-200">
             <InputMain class="flex-1" label="Image URL" v-model="userInfo.imageURL" placeholder="Image URL" />
         </div>
-        <div class="flex gap-24 py-6">
+        <div class="flex gap-32 py-6">
             <InputMain class="flex-1" label="Current Password" v-model="userInfo.currentPassword"
-                placeholder="Current Password" type="password" />
+                placeholder="Current Password" />
             <InputMain class="flex-1" label="New Password" v-model="userInfo.newPassword" placeholder="New Password"
                 type="password" />
             <InputMain class="flex-1" label="Confirm New Password" v-model="userInfo.confirmNewPassword"
